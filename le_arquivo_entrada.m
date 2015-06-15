289d0f839c0db25200307c2720da2476ddf12391
AntonioMateus <antoniocms1995@hotmail.com>
2015-06-15 18:23:41 -0300

    Update le_arquivo_entrada.m

--- le_arquivo_entrada.m
+++ le_arquivo_entrada.m
@@ -1,12 +1,7 @@
 function [ent] = le_arquivo_entrada( nome_entrada )
 %Metodo usado para ler o arquivo que contem os dados de entrada (os quais
-%podem ser dados de teste ou treinamento). Alem disso, os dados sao
-%normalizados para que fiquem no intervalo [0, 1]. 
+%podem ser dados de teste ou treinamento). 
     arquivo = fopen(nome_entrada, 'r');
     formatSpec = '%f\n'; 
-    ent = fscanf(arquivo, formatSpec);
-    for i=1:length(ent)
-        b = ent(i);
-        ent(i) = (b - min(ent))/(max(ent) - min(ent)); 
-    end         
+    ent = fscanf(arquivo, formatSpec);         
 end
